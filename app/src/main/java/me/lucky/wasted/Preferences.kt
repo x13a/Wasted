33d872d560d177a47d80d7f07f3f74e1116eb8d1
lucky <>
2021-11-21 16:57:03 +0300

    move authentication code to secure store

--- app/src/main/java/me/lucky/wasted/Preferences.kt
+++ app/src/main/java/me/lucky/wasted/Preferences.kt
@@ -3,15 +3,23 @@ package me.lucky.wasted
 import android.content.Context
 
 import androidx.core.content.edit
-import androidx.preference.PreferenceManager
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
-class Preferences(context: Context) {
+class Preferences(ctx: Context) {
     companion object {
         private const val SERVICE_ENABLED = "service_enabled"
         private const val CODE = "code"
     }
 
-    private val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+    private val mk = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    private val prefs = EncryptedSharedPreferences.create(
+        "s_shared_prefs",
+        mk,
+        ctx,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
+    )
 
     var isServiceEnabled: Boolean
         get() = prefs.getBoolean(SERVICE_ENABLED, false)
