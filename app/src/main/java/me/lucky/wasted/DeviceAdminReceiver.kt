85fcc22463a01cddebce83ce4c9b3a51359a531d
lucky <>
2022-02-16 12:35:31 +0300

    ref

--- app/src/main/java/me/lucky/wasted/DeviceAdminReceiver.kt
+++ app/src/main/java/me/lucky/wasted/DeviceAdminReceiver.kt
@@ -9,20 +9,16 @@ import android.widget.Toast
 class DeviceAdminReceiver : DeviceAdminReceiver() {
     override fun onPasswordFailed(context: Context, intent: Intent, user: UserHandle) {
         super.onPasswordFailed(context, intent, user)
-        onPasswordFailedInternal(context)
+        val prefs = Preferences(context)
+        if (!prefs.isServiceEnabled || prefs.maxFailedPasswordAttempts <= 0) return
+        val admin = DeviceAdminManager(context)
+        if (admin.getCurrentFailedPasswordAttempts() >= prefs.maxFailedPasswordAttempts)
+            admin.wipeData()
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
         super.onDisabled(context, intent)
         if (Preferences(context).isServiceEnabled)
             Toast.makeText(context, R.string.service_unavailable_popup, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun onPasswordFailedInternal(ctx: Context) {
-        val prefs = Preferences(ctx)
-        if (!prefs.isServiceEnabled || prefs.maxFailedPasswordAttempts <= 0) return
-        val admin = DeviceAdminManager(ctx)
-        if (admin.getCurrentFailedPasswordAttempts() >= prefs.maxFailedPasswordAttempts)
-            admin.wipeData()
     }
 }
