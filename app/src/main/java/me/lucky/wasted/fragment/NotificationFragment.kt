b5bebfe8382d91d1cbe2e5d9c4ed5c1b07c5b89b
lucky <lucky@lucky-book.local>
2022-07-10 09:07:12 +0300

    1.5.3
    add button to jump into device & app notifications setting
    add Polish translation, thanks to py3o4f4ifk5v9fko
    update Italian translation, thanks to Giovanni Donisi (@gdonisi + @giovannidonisi)

---
+++ app/src/main/java/me/lucky/wasted/fragment/NotificationFragment.kt
@@ -0,0 +1,31 @@
+package me.lucky.wasted.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import me.lucky.wasted.databinding.FragmentNotificationBinding
+
+class NotificationFragment : Fragment() {
+    private lateinit var binding: FragmentNotificationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        setup()
+        return binding.root
+    }
+
+    private fun setup() = binding.apply {
+        gotoButton.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
+        }
+    }
+}
